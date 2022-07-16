ebe4c3971020c3478451cc60714fe861d9497704
chulanpro-student <chulanpro5@gmail.com>
2022-07-17 04:50:04 +0700

    Update appearances

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Quote(props)
 {
     return (
-        <div class="bg-cyan-800 rounded-xl p-4 content-center m-4">
+        <div class="bg-cyan-500 rounded-xl p-4 content-center m-4">
 
             <div class="flex space gap-4">
                 <img class="rounded-lg ring-2 ring-gray-300 p-1 w-20 h-20 object-cover" src="https://www.jimdo.com/static/788abd633ed9f2088117678a650fc7c4/c76cf/steplist-author.jpg" />
