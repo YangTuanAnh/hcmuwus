45966c8ad8230e07b168fc74bb0813360ef6e189
YangTuanAnh <yangtuananh2003@gmail.com>
2022-07-17 09:38:08 +0700

    Update GreetingBox.jsx

--- src/components/GreetingBox/GreetingBox.jsx
+++ src/components/GreetingBox/GreetingBox.jsx
@@ -5,14 +5,14 @@ function GreetingBox(props)
     if (props.avatar == undefined)
     {
         return (
-            <div class="font-bold text-xl mx-8 my-4">
+            <div class="font-bold text-xl mx-8 my-4 pr-12">
                 {props.message}
             </div>
         )
     }
     return (
         <div class="flex m-4">
-            <div class="flex-1 font-bold text-5xl">{props.message}</div>
+            <div class="flex-1 font-bold text-5xl pr-12">{props.message}</div>
             <img class="auto rounded-lg ring-2 ring-gray-300 p-1 w-20 h-20 object-cover" src={props.avatar} />
         </div>
     )
