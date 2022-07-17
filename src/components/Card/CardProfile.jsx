b369c535d8b2782cd1080b9a24c9f618965ac324
YangTuanAnh <yangtuananh2003@gmail.com>
2022-07-17 09:06:00 +0700

    Update CardProfile.jsx

--- src/components/Card/CardProfile.jsx
+++ src/components/Card/CardProfile.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function CardProfile(props)
 {
     return (
-        <header class="p-8" style={{
+        <header style={{
             backgroundImage: `url(${props.headerImage})`
         }}>
-            <div class="bg-white rounded-xl shadow-xl m-8 pb-16 px-8">
+            <div class="bg-white rounded-xl shadow-xl pb-16 px-8 my-8">
                 <img class="rounded-full w-40 h-40 object-cover ring-2 p-2 ring-gray-200 origin-top -translate-y-8 m-auto" src={props.avatar} alt={props.name} />
                 <h2 class="font-bold text-3xl text-center">{props.name}</h2>
                 <p class="font-light text-lg text-center">{props.caption}</p>
