bf9b8fb27dd8b0e21b351c99ff53f8eda62cdb7a
YangTuanAnh <yangtuananh2003@gmail.com>
2022-07-17 09:20:17 +0700

    Fixed Nguyện Vọng in Profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Assistant from "../components/Assistant/Assistant";
 import CardMemory from "../components/Card/CardMemory";
 import CardProfile from "../components/Card/CardProfile";
+import Card from "../components/Card/Card";
+import Wishes from "../data/wishes.json"
+import CardAudio from "../components/Card/CardAudio";
 
 function Profile()
 {
@@ -13,6 +16,23 @@ function Profile()
                 caption="I'm Iron Man."
                 avatar="https://assets.puzzlefactory.pl/puzzle/387/881/original.jpg"
             />
+            <h2 class="text-3xl font-bold py-4 border-b border-gray-300 ">
+                Nguyện vọng
+            </h2>
+            <div class="grid lg:grid-cols-4 grid-cols-2">
+                {
+                    Wishes.map((task, index) =>
+                    {
+                        return (
+                            <Card
+                                taskTitle={task.taskTitle}
+                                headerImage={task.headerImage}
+                                id={index}
+                            />
+                        )
+                    })
+                }
+            </div>
             <h2 class="text-3xl font-bold py-4 border-b border-gray-300 ">
                 Di Vật
             </h2>
@@ -28,6 +48,11 @@ function Profile()
                     "https://www.funfactsabout.com/wp-content/uploads/2021/11/fun-facts-skiing.jpg",
                     "https://www.funfactsabout.com/wp-content/uploads/2021/11/fun-facts-skiing.jpg"
                 ]}
+            />
+            <CardAudio
+                caption="I love you 3000"
+                date="26/03/2019"
+                day="Thứ 7"
             />
             <h2 class="text-3xl font-bold py-4 border-b border-gray-300">
                 Trợ Lý
